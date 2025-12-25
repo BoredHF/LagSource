f85e9e816bc5ab852b3ff7a95bf1b864f7705dbf
Albert Roche <kadosutop@gmail.com>
2025-12-25 18:19:22 +0000

    Fix: target Paper API 1.20.6 for Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
 }
 
 tasks.processResources {
