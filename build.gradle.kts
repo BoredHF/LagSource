ae59fdc617d629752f53e03ba13edc2b7d341dc0
Albert Roche <kadosutop@gmail.com>
2025-12-25 18:16:56 +0000

    PR1: project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+}
+
+group = "com.lagsource"
+version = "0.1.0-SNAPSHOT"
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(17)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+}
+
+tasks.processResources {
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand("version" to project.version)
+    }
+}
---
+++ src/main/java/com/lagsource/LagSourcePlugin.java
@@ -0,0 +1,10 @@
+package com.lagsource;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LagSourcePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+}
